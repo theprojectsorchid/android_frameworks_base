9b3d4b143928f5e2014b36beb286d05eb31f279d
LorDClockaN <davor@losinj.com>
2016-01-21 23:05:00 +0200

    OrchidOs Buttons :  Power/reboot menu transparency (1/2)
    SDB: adapt for A12 as GlobalActionDialog got a sibling, GlobalActionsDialogLite.
    
    Change-Id: Ib703c2574aca9bc3c534a155d165f07b603c0535
    Signed-off-by: Escobar1945 <candaroglu197878@gmail.com>
    Co-Authored-By: Danny Lin <danny@kdrag0n.dev>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5291,6 +5291,18 @@ public final class Settings {
        public static final String BATTERY_LIGHT_REALLYFULL_COLOR = "battery_light_reallyfull_color";
 
         /**
+         * Transparent power menu and dialogs
+         * @hide
+         */
+        public static final String TRANSPARENT_POWER_MENU = "transparent_power_menu";
+
+        /**
+         * Dim amount around power/reboot menu dialogs
+         * @hide
+         */
+        public static final String TRANSPARENT_POWER_DIALOG_DIM = "transparent_power_dialog_dim";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -5490,6 +5502,8 @@ public final class Settings {
             PRIVATE_SETTINGS.add(BATTERY_LIGHT_MEDIUM_COLOR);
             PRIVATE_SETTINGS.add(BATTERY_LIGHT_FULL_COLOR);
             PRIVATE_SETTINGS.add(BATTERY_LIGHT_REALLYFULL_COLOR);
+            PRIVATE_SETTINGS.add(TRANSPARENT_POWER_MENU);
+            PRIVATE_SETTINGS.add(TRANSPARENT_POWER_DIALOG_DIM);
         }
 
         /**
