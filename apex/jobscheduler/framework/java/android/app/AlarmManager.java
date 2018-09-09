054b013155bb0c654c608761f939c5ca04139017
faust93 <monumentum@gmail.com>
2018-09-09 21:10:06 +0800

    OrchidOS : Add AlarmsBlocker [1/2]
    Made by faust93. Based on WakelockBlocker by maxwen
    
    neobuddy89: Moved to global settings.
    
    Change-Id: Ida3075aa251443463b86b4bbf2c523d036fba65d

--- apex/jobscheduler/framework/java/android/app/AlarmManager.java
+++ apex/jobscheduler/framework/java/android/app/AlarmManager.java
@@ -1338,6 +1338,17 @@ public class AlarmManager {
         }
     }
 
+    /** @hide */
+    public String getSeenAlarms() {
+        try {
+            if (mService != null) {
+                return mService.getSeenAlarms();
+            }
+        } catch (RemoteException e) {
+        }
+        return null;
+    }
+
     /**
      * Gets information about the next alarm clock currently scheduled.
      *
