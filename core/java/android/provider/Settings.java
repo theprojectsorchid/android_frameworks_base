2bc0c80ecc942df3ab609fd3b1939c4ebad0c3bf
Ashwin R C <ashwin2001achu@gmail.com>
2020-05-27 10:26:16 +0000

    OrchidOs : allow disable of screenshot shutter sound [1/2]
    [SahilSonar - POSP]
    
     - Forward port to android-12.0.0
    
    Change-Id: I381c351131241e45ddb6049706d6c302c2eee946
    
    base: allow disable of screenshot shutter sound [1/2]
    
    [SahilSonar - POSP]
     - Forward port to android-12.0.0
    
    Change-Id: I47d52bba21170118af87d35376d81d7569587a2f
    
    SystemUI: Screenshots: Refactor shutter sound logic
    
    * Commit 2f09bacae02fe7a99d62355331c3a26b83752a6a introduced some
      duplicated code
    * Move it to an own method to reduce the footprint in the AOSP code parts
      and to reduce duplication
    
    [SahilSonar - POSP]
      - Forward port to androi-12.0.0
    
    Change-Id: I57eaaee4db401d16cc6ef65c68604cdb4053ca01
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    
    SystemUI: Fix shutter sound
    
    * When shutter sound for camera is forced on as required in some states,
      (config_camera_sound_forced, set via mcc/mnc), we also want to
      (or should) play it when a screenshot is taken from the preview
      instead of an actual picture
    * This change is loosely based on
      https://android-review.googlesource.com/c/platform/frameworks/base/+/1517742/
      but uses publicly available APIs
    
    Testing:
    - Set config_camera_sound_forced to true and push a build to device
    - Turn down all stream volumes to muted
    - Take screenshot of any normal screen
      -> No sound played
    - Open camera, take screenshot
      -> Sound played
    - Turn up volume and repeat the screenshots
      -> Sound played in all cases
    
    [SahilSonar - POSP]
     - Forward port to android-12.0.0
    
    Change-Id: Iacbb577c64f73b79cdfae7fb0487fc6a34ffe41c
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5212,6 +5212,11 @@ public final class Settings {
         public static final String LESS_BORING_HEADS_UP = "less_boring_heads_up";
 
         /**
+         * @hide
+         */
+        public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
