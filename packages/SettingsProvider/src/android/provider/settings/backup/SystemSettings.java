243ff612b7a1a61888cda4ada210cd3917d509b8
Ido Ben-Hur <idoybh2@gmail.com>
2020-10-09 15:16:28 +0300

    OrchidOs Settings : Allow hiding notification headers [1/2]
    Gimme back my space, thank you
    
    Change-Id: I5f63a384d2bd2052d4a8a9d49aff1456d743367f
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -89,5 +89,6 @@ public class SystemSettings {
         Settings.System.NETWORK_TRAFFIC_STATE,
         Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD,
         Settings.System.FP_WAKE_UNLOCK,
+        Settings.System.NOTIFICATION_HEADERS,
     };
 }
