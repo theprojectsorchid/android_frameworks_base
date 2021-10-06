1f57984e6f55d219cc426ca9fb42aba1cf445df1
Danny Lin <danny@kdrag0n.dev>
2021-10-05 21:01:09 -0700

    OrchidOs Security SystemUI :  Require unlocking to use sensitive QS tiles optional [1/2]
    - Airplane mode
    - Battery saver
    - Bluetooth
    - Cellular
    - DataSaver
    - Hotspot
    - NFC
    - Rotation lock
    - Wi-Fi
    - Work mode
    
    This is the same idea as the following commits from GrapheneOS, but
    simplified to reduce changes made to each individual QS tile:
    
    commit 45670099ed63c9fbde6ff1bd13d27b674416d21d
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:22:41 2020 -0700
    
        require unlocking to use work mode tile
    
    commit 4b7c9559dfba84e10142fcd53b3edac9c75b0748
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:21:16 2020 -0700
    
        require unlocking to use battery saver tile
    
    commit f45c24a4728531441c60320eda0a4fce4285de06
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:19:36 2020 -0700
    
        require unlocking to use cellular quick tile
    
    commit c18f6f868aee5b8f27fcc1966089f814f9f89ab2
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:15:46 2020 -0700
    
        require unlocking to use hotspot quick tile
    
    commit 442c94e1ee499eed467863362f8eb62bb88027a4
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:13:58 2020 -0700
    
        require unlocking to use data saver quick tile
    
    commit cf84259708f9bf650c8494e16025725b1bfa5e23
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:11:58 2020 -0700
    
        require unlocking to use rotation lock quick tile
    
    commit 61eb5a6392c53e4d44cb91cdb4df27c3055ef439
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:08:33 2020 -0700
    
        require unlocking to use Wi-Fi quick tile
    
    commit ad122f0a5a46e2445b5866d3ff83fdc7623b76f6
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:05:18 2020 -0700
    
        require unlocking to use airplane mode quick tile
    
    commit a3686b6f905fb734b3a4f6db545e8a3ef3fc3cc7
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 17:02:20 2020 -0700
    
        require unlocking to use Bluetooth quick tile
    
    commit 3046535a40f53835eb0b57d4c12a29828ba90f30
    Author: inthewaves <inthewaves@pm.me>
    Date:   Thu Sep 10 16:47:59 2020 -0700
    
        require unlocking to use NFC quick tile
    
    Change-Id: I515f4bdd30f84a4a76f4807afcf3cc5a3ce42fa9
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>
    
    Change-Id: I355ef643fc046ce50f40924db130ad212fea5800
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    
    Update Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5407,7 +5407,7 @@ public final class Settings {
          * @hide
          */
         public static final String USE_TILES_ON_SECURE_KEYGUARD = "use_tiles_on_secure_keyguard";
-
+        
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
