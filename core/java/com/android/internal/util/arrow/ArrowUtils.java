11fe8256683940bac6456621b4bdf5bdba297a91
SpiritCroc <spiritcroc@gmail.com>
2021-11-07 14:46:02 +0000

    OrchidOs arrowUtils: Import restart systemui api
    Change-Id: I225b2dffe92816d4267b5041621232408e2d613d
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- core/java/com/android/internal/util/arrow/ArrowUtils.java
+++ core/java/com/android/internal/util/arrow/ArrowUtils.java
@@ -16,11 +16,16 @@
 
 package com.android.internal.util.arrow;
 
+import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.app.IActivityManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.input.InputManager;
 import android.os.Handler;
+import android.os.AsyncTask;
 import android.os.Looper;
 import android.os.PowerManager;
 import android.os.RemoteException;
@@ -31,6 +36,8 @@ import android.view.WindowManagerGlobal;
 import android.view.InputDevice;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
+import android.view.WindowManagerGlobal;
+import com.android.internal.R;
 
 import com.android.internal.statusbar.IStatusBarService;
 
@@ -117,6 +124,50 @@ public class ArrowUtils {
                     // do nothing.
                 }
             }
+        }
+    }
+
+    public static void restartSystemUi(Context context) {
+        new RestartSystemUiTask(context).execute();
+    }
+
+    public static void showSystemUiRestartDialog(Context context) {
+        new AlertDialog.Builder(context)
+                .setTitle(R.string.systemui_restart_title)
+                .setMessage(R.string.systemui_restart_message)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        restartSystemUi(context);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .show();
+    }
+
+    private static class RestartSystemUiTask extends AsyncTask<Void, Void, Void> {
+        private Context mContext;
+
+        public RestartSystemUiTask(Context context) {
+            super();
+            mContext = context;
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                ActivityManager am =
+                        (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
+                IActivityManager ams = ActivityManager.getService();
+                for (ActivityManager.RunningAppProcessInfo app: am.getRunningAppProcesses()) {
+                    if ("com.android.systemui".equals(app.processName)) {
+                        ams.killApplicationProcess(app.processName, app.uid);
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
         }
     }
 }
