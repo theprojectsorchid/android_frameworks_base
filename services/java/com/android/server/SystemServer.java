839da925b4911870c3811920fc7269b6868a3fe6
LibXZR <i@xzr.moe>
2022-02-09 09:58:48 +0800

    OrchidOs MicroG : Add support for gms switch
    This allows us to enable or disable Google Mobile Services components
    by changing a single setting. Also, it provides basic capability to
    fake gms installation status that reports "not installed" rather than
    "disabled", because some apps will show a notification when gms is
    disabled but stay quite when gms is totally unavailable.
    
    It has been designed to support multi-user, which means you can toggle
    different gms status for every single user.
    
    Change-Id: I7442eb094cea1a191abce02edbf905fac0fb20ad
    Signed-off-by: LibXZR <i@xzr.moe>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -208,6 +208,8 @@ import dalvik.system.VMRuntime;
 
 import com.google.android.startop.iorap.IorapForwardingService;
 
+import ink.kaleidoscope.server.GmsManagerService;
+
 import java.io.File;
 import java.io.FileDescriptor;
 import java.io.IOException;
@@ -379,6 +381,8 @@ public final class SystemServer implements Dumpable {
             "com.android.server.media.MediaCommunicationService";
     private static final String APP_COMPAT_OVERRIDES_SERVICE_CLASS =
             "com.android.server.compat.overrides.AppCompatOverridesService$Lifecycle";
+    private static final String GMS_MANAGER_SERVICE_SERVICE_CLASS =
+            "ink.kaleidoscope.server.GmsManagerService";
 
     private static final String ROLE_SERVICE_CLASS = "com.android.role.RoleService";
     private static final String GAME_MANAGER_SERVICE_CLASS =
@@ -2655,7 +2659,10 @@ public final class SystemServer implements Dumpable {
         mSystemServiceManager.startService(APP_COMPAT_OVERRIDES_SERVICE_CLASS);
         t.traceEnd();
 
-        // These are needed to propagate to the runnable below.
+        t.traceBegin("StartGmsManagerService");
+        mSystemServiceManager.startService(GMS_MANAGER_SERVICE_SERVICE_CLASS);
+        t.traceEnd();
+
         final NetworkManagementService networkManagementF = networkManagement;
         final NetworkStatsService networkStatsF = networkStats;
         final NetworkPolicyManagerService networkPolicyF = networkPolicy;
