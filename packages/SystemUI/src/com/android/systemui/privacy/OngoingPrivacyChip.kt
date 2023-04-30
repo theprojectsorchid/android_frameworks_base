5927f17d54b4e4dc4ffe383f959abc841ba4409a
theprojectsorchid <126561359+theprojectsorchid@users.noreply.github.com>
2023-04-30 20:49:11 +0100

    Revert "OrchidOs statusbar : PrivacyChips: Redesign the chips to match guidelines"
    This reverts commit c59961248ca5b3548ba8e1638f51b2aec0ffcf7c.

--- packages/SystemUI/src/com/android/systemui/privacy/OngoingPrivacyChip.kt
+++ packages/SystemUI/src/com/android/systemui/privacy/OngoingPrivacyChip.kt
@@ -30,26 +30,11 @@ class OngoingPrivacyChip @JvmOverloads constructor(
     defStyleRes: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttrs, defStyleRes) {
 
-    private val iconMarginExpanded = context.resources.getDimensionPixelSize(
-                    R.dimen.ongoing_appops_chip_icon_margin_expanded)
-    private val iconMarginCollapsed = context.resources.getDimensionPixelSize(
-                    R.dimen.ongoing_appops_chip_icon_margin_collapsed)
-    private val iconSize =
-            context.resources.getDimensionPixelSize(R.dimen.ongoing_appops_chip_icon_size)
-    private val iconColor = context.resources.getColor(
-            R.color.status_bar_clock_color, context.theme)
-    //private val sidePadding =
-    //        context.resources.getDimensionPixelSize(R.dimen.ongoing_appops_chip_side_padding)
-    //private val backgroundDrawable = context.getDrawable(R.drawable.privacy_chip_bg)
+    private var iconMargin = 0
+    private var iconSize = 0
+    private var iconColor = 0
+
     private lateinit var iconsContainer: LinearLayout
-    private lateinit var back: FrameLayout
-    var expanded = false
-        set(value) {
-            if (value != field) {
-                field = value
-                updateView()
-            }
-        }
 
     var privacyList = emptyList<PrivacyItem>()
         set(value) {
@@ -66,10 +51,7 @@ class OngoingPrivacyChip @JvmOverloads constructor(
     }
 
     // Should only be called if the builder icons or app changed
-    private fun updateView() {
-        //back.background = if (expanded) backgroundDrawable else null
-        //val padding = if (expanded) sidePadding else 0
-        //back.setPaddingRelative(padding, 0, padding, 0)
+    private fun updateView(builder: PrivacyChipBuilder) {
         fun setIcons(chipBuilder: PrivacyChipBuilder, iconsContainer: ViewGroup) {
             iconsContainer.removeAllViews()
             chipBuilder.generateIcons().forEachIndexed { i, it ->
